8f2df7df27f805b9c70dd95728d63ce430b607d5
Yeon <giojio825@gmail.com>
2024-01-02 16:09:45 +0700

    main

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("halooo");
+function loadPage(page) {
+  fetch(page)
+    .then((response) => response.text())
+    .then((html) => {
+      document.getElementById("content").innerHTML = html;
+    })
+    .catch((error) => console.log("Error:", error));
+}
